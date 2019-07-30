dd8cc073afadf11e22b7bbf20e1eaa67a790a54a
Sachin Suthariya <sachinsuthariya@gmail.com>
2019-07-30 15:15:19 +0530

    text filter added

--- src/app/restaurant/admin/list-restaurant/list-restaurant.component.ts
+++ src/app/restaurant/admin/list-restaurant/list-restaurant.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminServiceService } from '../admin-service.service';
-import { element } from 'protractor';
+// import { element } from 'protractor';
 
 @Component({
   selector: 'app-list-restaurant',
@@ -10,6 +10,7 @@ import { element } from 'protractor';
 export class ListRestaurantComponent implements OnInit {
 
   restaurantList = [];
+  restFilter = [];
   // search;
   // resName;
   constructor(private adminService: AdminServiceService) {
@@ -20,8 +21,10 @@ export class ListRestaurantComponent implements OnInit {
   // search_rest: string;
   search(event) {
     console.log(event.srcElement.value, "evnt of searchh");
-    // let search = event.srcElement.value;
-    // this.restaurantList = this.restaurantList.filter(element => element === search);
+    let search = event.srcElement.value;
+    // this.restaurantList = this.restFilter.filter(ele => (ele.restaurantName.toLowerCase(), search.toLowerCase()));
+    this.restaurantList = this.restFilter.filter(ele => ele.restaurantName.toLowerCase().includes(search.toLowerCase()));
+    console.log("restuarant lisa after  filter", this.restaurantList, this.restFilter);
 
 
 
@@ -41,6 +44,7 @@ export class ListRestaurantComponent implements OnInit {
         console.log(res, "list restaurant ");
         let response = res;
         this.restaurantList = response["body"][0];
+        this.restFilter = this.restaurantList;
 
         // console.log(this.restaurantList);
 
